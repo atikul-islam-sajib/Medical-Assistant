8c10b25b57a2a3d009d7e37cf61c91a926f23960
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-04-18 19:02:16 +0200

    device init is done in the utils file

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import yaml
+import torch
 import joblib
 
 sys.path.append("./src/")
@@ -11,9 +12,19 @@ def dump_files(value=None, filename=None):
         raise ValueError("Either values or filename must be provided".capitalize())
     else:
         joblib.dump(value=value, filename=filename)
-        
+
+
 def load_files(filename: str = None):
     if filename is None:
         raise ValueError("Filename must be provided".capitalize())
     else:
         return joblib.load(filename=filename)
+
+
+def device_init(device: str = "cuda"):
+    if device == "cuda":
+        return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    elif device == "mps":
+        return torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    else:
+        return torch.device("cpu")
