ad208434ff98915d7f3a9f431d5d2bb3d180fec0
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-04-18 20:58:18 +0200

    dataloader is completed without argsparse

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,12 +1,18 @@
 import os
 import sys
 import torch
+import zipfile
+import cv2
 import torch.nn as nn
+from PIL import Image
 from sklearn.model_selection import train_test_split
 from torchvision import transforms
-from torch.utils.data import dataloader
+from tqdm import tqdm
+from torch.utils.data import DataLoader
 
 sys.path.append("./src/")
+
+from utils import dump_files
 
 
 class Loader:
@@ -23,24 +29,144 @@ class Loader:
         self.image_size = image_size
         self.batch_size = batch_size
         self.split_size = split_size
-        
+
+        self.train_images = []
+        self.train_labels = []
+        self.valid_images = []
+        self.valid_labels = []
+
     def unzip_folder(self):
-        pass
-    
+        if not os.path.exists("./data/processed"):
+            os.makedirs("./data/processed")
+
+        with zipfile.ZipFile(file=self.image_path, mode="r") as file:
+            file.extractall(path="./data/processed")
+
+        print("""Extracted file saved in the "./data/processed" folder""")
+
     def split_dataset(self, **kwargs):
-        pass
-    
-    def image_transforms(self):
-        pass
-    
+        X = kwargs["X"]
+        y = kwargs["y"]
+
+        if not isinstance(X, list) and not isinstance(y, list):
+            raise ValueError("Invalid data type".capitalize())
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=self.split_size, random_state=42
+        )
+
+        return {
+            "X_train": X_train,
+            "X_test": X_test,
+            "y_train": y_train,
+            "y_test": y_test,
+        }
+
+    def image_transforms(self, type: str = "RGB"):
+        if type == "RGB":
+            return transforms.Compose(
+                [
+                    transforms.Resize((self.image_size, self.image_size)),
+                    transforms.ToTensor(),
+                    transforms.CenterCrop((self.image_size, self.image_size)),
+                    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+                ]
+            )
+        else:
+            return transforms.Compose(
+                [
+                    transforms.Resize((self.image_size, self.image_size)),
+                    transforms.ToTensor(),
+                    transforms.CenterCrop((self.image_size, self.image_size)),
+                    transforms.Normalize([0.5], [0.5]),
+                ]
+            )
+
+    def extract_features(self):
+        train_path = "./data/processed/Training"
+        valid_path = "./data/processed/Testing"
+
+        class_names = ["glioma", "meningioma", "pituitary", "notumor"]
+
+        for path in [train_path, valid_path]:
+            for label in class_names:
+                image_path = os.path.join(path, label)
+                for image in tqdm(
+                    os.listdir(image_path), desc="Extracting images".title()
+                ):
+                    single_image_path = os.path.join(image_path, image)
+                    if not single_image_path.endswith(("png", "jpg", "jpeg")):
+                        raise ValueError("Invalid image format")
+
+                    image = cv2.imread(single_image_path)
+                    image = Image.fromarray(image)
+                    image = self.image_transforms(
+                        "GRAY" if self.image_channels == 1 else "RGB"
+                    )(image)
+
+                    if path == train_path:
+                        self.train_images.append(image)
+                        self.train_labels.append(class_names.index(label))
+                    else:
+                        self.valid_images.append(image)
+                        self.valid_labels.append(class_names.index(label))
+
+        assert len(self.train_images) == len(self.train_labels)
+        assert len(self.valid_images) == len(self.valid_labels)
+
+        train_dataset = self.split_dataset(X=self.train_images, y=self.train_labels)
+
+        return {
+            "X_train": torch.stack(train_dataset["X_train"][:400]).float(),
+            "X_test": torch.stack(train_dataset["X_test"][:400]).float(),
+            "y_train": torch.tensor(train_dataset["y_train"][:100], dtype=torch.long),
+            "y_test": torch.tensor(train_dataset["y_test"][:100], dtype=torch.long),
+            "valid_images": torch.stack(self.valid_images[:50]).float(),
+            "valid_labels": torch.tensor(self.valid_labels[:50], dtype=torch.long),
+        }
+
     def create_dataloader(self):
-        pass
-    
+        dataset = self.extract_features()
+
+        train_dataloader = DataLoader(
+            dataset=list(zip(dataset["X_train"], dataset["y_train"])),
+            batch_size=self.batch_size,
+            shuffle=True,
+        )
+        test_dataloader = DataLoader(
+            dataset=list(zip(dataset["X_test"], dataset["y_test"])),
+            batch_size=self.batch_size,
+            shuffle=True,
+        )
+        valid_dataloader = DataLoader(
+            dataset=list(zip(dataset["valid_images"], dataset["valid_labels"])),
+            batch_size=self.batch_size,
+            shuffle=True,
+        )
+
+        for value, filename in tqdm(
+            [
+                (train_dataloader, "train_dataloader.pkl"),
+                (test_dataloader, "test_dataloader.pkl"),
+                (valid_dataloader, "valid_dataloader.pkl"),
+            ],
+            desc="Saving dataloaders".title(),
+        ):
+            dump_files(
+                value=value, filename=os.path.join("./data/processed/", filename)
+            )
+
+        print("Files saved in the folder ./data/processed/")
+
+
 if __name__ == "__main__":
     loader = Loader(
         image_path="./data/raw/dataset.zip",
-        image_channels=3,
+        image_channels=1,
         image_size=224,
-        batch_size=64,
-        split_size=0.25,
+        batch_size=16,
+        split_size=0.30,
     )
+
+    loader.unzip_folder()
+    loader.create_dataloader()
