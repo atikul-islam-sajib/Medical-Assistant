5e70ded7376b92f74800978e114c19871bd5aad5
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-04-18 19:00:52 +0200

    load files is added in the utils

--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,9 @@ def dump_files(value=None, filename=None):
         raise ValueError("Either values or filename must be provided".capitalize())
     else:
         joblib.dump(value=value, filename=filename)
+        
+def load_files(filename: str = None):
+    if filename is None:
+        raise ValueError("Filename must be provided".capitalize())
+    else:
+        return joblib.load(filename=filename)
