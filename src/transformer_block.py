5361b0ecdb9add508e4a83d6ceda2f17ec05e316
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-04-19 14:45:42 +0200

    transformer encoder block is added in the src

--- src/transformer_block.py
+++ src/transformer_block.py
@@ -15,6 +15,7 @@ class TransformerEncoderBlock(nn.Module):
     def __init__(
         self,
         nhead: int = 8,
+        d_model: int = 768,
         dim_feedforward: int = 2048,
         dropout: float = 0.1,
         activation: str = "gelu",
@@ -23,18 +24,51 @@ class TransformerEncoderBlock(nn.Module):
     ):
         super(TransformerEncoderBlock, self).__init__()
         self.nheads = nhead
+        self.d_model = d_model
         self.dim_feedforward = dim_feedforward
         self.dropout = dropout
         self.activation = activation
         self.layer_norm_eps = layer_norm_eps
         self.bias = bias
-        
-        
+
+        self.multi_head_attention = MultiHeadAttentionLayer(
+            nhead=self.nheads,
+            dimension=self.d_model,
+        )
+
+        self.layer_norm1 = LayerNormalization(
+            normalized_shape=self.d_model, eps=self.layer_norm_eps
+        )
+        self.layer_norm2 = LayerNormalization(
+            normalized_shape=self.d_model, eps=self.layer_norm_eps
+        )
+        self.feed_forward_network = FeedForwardNeuralNetwork(
+            d_model=self.d_model,
+            dim_feedforward=self.dim_feedforward,
+            dropout=self.dropout,
+            activation=self.activation,
+        )
+        self.dropout1 = nn.Dropout(p=self.dropout)
+        self.dropout2 = nn.Dropout(p=self.dropout)
+
     def forward(self, x: torch.Tensor):
         if not isinstance(x, torch.Tensor):
             raise TypeError("Input must be a torch.Tensor")
         else:
-            ...
+            residual = x
+            
+            x = self.multi_head_attention(x)
+            x = self.dropout1(x)
+            x = torch.add(x, residual)
+            x = self.layer_norm1(x)
+            
+            residual = x
+            
+            x = self.feed_forward_network(x)
+            x = torch.add(x, residual)
+            x = self.layer_norm2(x)
+
+            return x
 
 if __name__ == "__main__":
     transformer = TransformerEncoderBlock(
