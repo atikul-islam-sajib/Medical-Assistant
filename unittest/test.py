3495b7d173572c84795c27bf5f1d337a71c05356
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-04-19 13:17:00 +0200

    multihead attention passed the unittest

--- unittest/test.py
+++ unittest/test.py
@@ -7,15 +7,17 @@ import torch.nn as nn
 sys.path.append("./src/")
 
 from patch_embedding import PatchEmbedding
+from multi_head_attention_layer import MultiHeadAttentionLayer
 
 
 class UnitTest(unittest.TestCase):
     def setUp(self):
         self.batch_size = 64
-        self.image_channels = 3
+        self.image_channels = 1
         self.image_size = 224
         self.patch_size = 16
-        self.embedding_dimension = 768
+        self.nheads = 8
+        self.embedding_dimension = 256
 
         self.patch_embedding = PatchEmbedding(
             image_channels=self.image_channels,
@@ -28,6 +30,10 @@ class UnitTest(unittest.TestCase):
             (self.batch_size, self.image_channels, self.image_size, self.image_size)
         )
 
+        self.multihead_attention = MultiHeadAttentionLayer(
+            nheads=self.nheads, dimension=self.embedding_dimension
+        )
+
     def test_patch_embedding(self):
         self.assertEqual(
             self.patch_embedding(self.images).size(),
@@ -36,6 +42,12 @@ class UnitTest(unittest.TestCase):
                 (self.image_size // self.patch_size) ** 2,
                 self.embedding_dimension,
             ),
+        )
+
+    def test_multi_head_attention(self):
+        self.assertEqual(
+            self.multihead_attention(x=self.patch_embedding(x=self.images)).size(),
+            self.patch_embedding(x=self.images).size(),
         )
 
 
