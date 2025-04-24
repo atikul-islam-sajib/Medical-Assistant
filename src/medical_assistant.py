87a0574f8c4e3428b2b43414e7dbca4cdfd4338e
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-04-24 10:46:51 +0200

    medical assiatant prototype is added

---
+++ src/medical_assistant.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import torch
+import torch.nn as nn
+
+sys.path.append("./src/")
+
+class MedicalAssistant():
+    def __init__(self):
+        pass
+    
+    def load_model(self):
+        pass
+    
+    def chat(self):
+        pass
+    
+    
+if __name__ == "__main__":
+    assistant = MedicalAssistant()
+    assistant.load_model()
+    assistant.chat()
